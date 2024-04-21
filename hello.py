b7886ac3e2c5a97809b53409e54108207b12d940
abdullmustyy <abdulmusty03@outlook.com>
2024-04-21 11:07:56 +0100

    chore: classes and pointers

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello, world!")
+num_1 = 11
+
+print("Num 1 is", num_1)
+print(id(num_1))
+
+num_1 = 22
+print("Num 1 is now", num_1)
+print(id(num_1))
+
+nmu_2 = num_1
+print("Num 2 is", nmu_2)
+print(id(nmu_2))
