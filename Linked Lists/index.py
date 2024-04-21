2c46cfc66f348d8370781f87dad1c3d02a47196e
abdullmustyy <abdulmusty03@outlook.com>
2024-04-21 17:58:25 +0100

    chore: add pop method to linked list

--- Linked Lists/index.py
+++ Linked Lists/index.py
@@ -34,9 +34,39 @@ class LinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        temp = self.head
+        pre = self.head
+
+        if self.length == 0:
+            return None
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return temp.value
+
 
 new_linked_list = LinkedList(1)
 
 new_linked_list.append(2)
+new_linked_list.append(3)
+# new_linked_list.append(4)
+# new_linked_list.append(5)
+# new_linked_list.append(6)
 
-new_linked_list.print_list()
+print(new_linked_list.pop())
+print(new_linked_list.pop())
+print(new_linked_list.pop())
+print(new_linked_list.pop())
+
+# new_linked_list.print_list()
